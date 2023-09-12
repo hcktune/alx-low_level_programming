0fcc1f3344e3e3617e3b681646f4f5671bdc2416
root <mohammed.aymane.montassir@gmail.com>
2023-09-12 13:48:56 -0700

    return in one single line

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower  - check for lowercase character
+ * @c: character
+ * Return: 1 if c is lowercase, otherwise 0
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
