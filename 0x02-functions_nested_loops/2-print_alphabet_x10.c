0eee16364c60cbee486d52b42c97db3b1bfa1ab1
root <mohammed.aymane.montassir@gmail.com>
2023-09-12 12:40:41 -0700

    add 2-main, 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  prints 10 times the alphabet in lowercase.
+ *
+ * Output: abcdefghij
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		print_alphabet(void);
+	}
+
+	_putchar('\n');
+}
