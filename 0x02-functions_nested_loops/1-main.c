9b70800076a24cd76e0f466e848c8793ffaedb0e
root <mohammed.aymane.montassir@gmail.com>
2023-09-12 11:32:42 -0700

    print all lower case alphaber using for i=a ; i <= z;

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/*
+ * main - Entry point
+ * 
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
-/*
+/**
  * _putchar - output one character based on ASCII code given
  * @c: The character 
  * 
@@ -8,7 +8,25 @@
  * otherwise -1
  */
 
+/**
+ * print_alphabet - print from alphabet 'a' to 'z' using for loop
+ *
+ * Output: abcdefghijklmnopqrstuvwxyz
+ */
+
 int _putchar(char c)
 {
 	return (write(1,&c,1));
+}
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+	
+	_putchar('\n');
 }
