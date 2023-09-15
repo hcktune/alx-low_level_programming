1bfbd81fec97aeb92f05fd3864bb65c575d37cc1
root <mohammed.aymane.montassir@gmail.com>
2023-09-15 15:44:34 -0700

    i / 10 + 48

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - print more numbers
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0 ; j < 10 ; j++)
+	{
+		for (i = 0 ; i < 15 ; i++)
+		{
+		if (i > 9)
+			_putchar(i / 10 + 48);
+
+		_putchar(i % 10 + 48);
+		}
+
+	_putchar('\n');
+	}
+}
