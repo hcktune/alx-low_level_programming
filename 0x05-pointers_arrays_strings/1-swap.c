d5933aa11554c3dcae06db2be0f7925e940b2307
root <mohammed.aymane.montassir@gmail.com>
2023-09-18 14:21:40 -0700

    update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,16 @@
 #include "main.h"
-
 /**
- * swap_int - swap two Variables
- * @a: first integer
- * @b: second integer
- * Return: swaping values
+ * swap_int - Function to swap
+ *
+ * @a: Integer
+ * @b: Integer
+ *
+ * Return: Void
  */
 
 void swap_int(int *a, int *b)
 {
-	int tmp = *a;
-
-	*a = *b;
-	b = tmp;
+int temp = *a;
+*a = *b;
+*b = temp;
 }
