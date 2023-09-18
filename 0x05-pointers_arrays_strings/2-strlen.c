1c97d38d801e06f6efef1a559ddc8ed73ad28f15
root <mohammed.aymane.montassir@gmail.com>
2023-09-18 14:30:16 -0700

    adding while(s[i] != 0)

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate the string length
+ * @str: string
+ * Return: string length = number
+ */
+
+int _strlen(char *str)
+{
+	int i;
+	
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
