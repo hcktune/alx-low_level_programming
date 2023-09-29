a290438cd23492b3a3f8b35df2e88330dc11998d
root <mohammed.aymane.montassir@gmail.com>
2023-09-28 19:40:26 -0700

    add putchar file

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes a single char to the STDOUT
+ * @c: the char
+ * Return: On success 1.
+ */
+
+int _putchar(char *c)
+{
+	return (write(1, c, 1));
+}
