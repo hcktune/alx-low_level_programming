d6f5509f2f73fdfabd81f3041c25c5f82d2d318b
root <mohammed.aymane.montassir@gmail.com>
2023-09-29 07:05:07 -0700

    fix betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,26 +4,28 @@
  * _sqrt_recursion - return natural square root of number
  * -1 for negative or does not have a natural square root
  * @n: number to be sqrt
- * @i: number of iteration
  * Return: sqrt values otherwise -1
  */
 
 int _sqrt_recursion(int n)
 {
-    return _sqrt_recursion_helper(n, 1);
+	return (_sqrt_recursion_helper(n, 1));
 }
+
+/**
+ * _sqrt_recursion_helper - calculate the sqrt
+ * @n: number to be squared
+ * @i: number of occurrences
+ * Return: number of occurences
+ */
 
 int _sqrt_recursion_helper(int n, int i)
 {
-    if (n < 0 || (i * i) > n)
-    {
-        return (-1);
-    }
-    else if ((i * i) == n)
-    {
-        return (i);
-    }
+	if (n < 0 || (i * i) > n)
+		return (-1);
+	else if ((i * i) == n)
+		return (i);
 
-    return _sqrt_recursion_helper(n, i + 1);
+	return (_sqrt_recursion_helper(n, i + 1));
 }
 
