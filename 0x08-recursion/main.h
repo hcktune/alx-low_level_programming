684a7d78175d78dd82451c1ec28b0364903acc42
root <mohammed.aymane.montassir@gmail.com>
2023-09-29 06:44:49 -0700

    fix main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,4 +3,6 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
-int _sqrt_recursion(int n, int i);
+int _sqrt_recursion_helper(int i, int n);
+int _sqrt_recursion(int n,);
+
