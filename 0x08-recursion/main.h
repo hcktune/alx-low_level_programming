61fa857222f8e3c7816c322979698e99a9bd5718
root <mohammed.aymane.montassir@gmail.com>
2023-09-29 10:07:07 -0700

    fix 3

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,4 +6,4 @@ int factorial(int n);
 int _sqrt_recursion_helper(int i, int n);
 int _sqrt_recursion(int n);
 int recursive_prime_helper(int n, unsigned int i);
-int is_prime_number(int i, int n);
+int is_prime_number(int n);
