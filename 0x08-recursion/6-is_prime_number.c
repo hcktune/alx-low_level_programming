a56c3c9d3ebd9b1a88fa0f2587e2923fb7f8634b
root <mohammed.aymane.montassir@gmail.com>
2023-09-29 10:01:35 -0700

    recursive and bettry fix

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,20 +8,21 @@
 
 int is_prime_number(int n)
 {
-	return (recursive_helper(n, 2);
+	return (recursive_helper(n, 2));
 }
 
 /**
- * recursive_prime_helper - Find the prime numbersigr
+ * recursive_prime_helper - Find the prime number
  * @n: number to find
+ * @i: number of iteration
  * Return: 1 if prime, 0 if not found
  */
 
-int recursive_helper(int n, int i)
+int recursive_prime_helper(int n, unsigned int i)
 {
 	if (n < 0)
 		return (0);
-	
+
 	else if (n % i == 0)
 		return (0);
 
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,4 +5,5 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _sqrt_recursion_helper(int i, int n);
 int _sqrt_recursion(int n);
-
+int recursive_prime_helper(int n, unsigned int i);
+int is_prime_number(int i, int n);
