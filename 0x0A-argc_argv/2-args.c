0d32485bef2fc08eb3d9e5cd5947d8ab73d14ddb
root <mohammed.aymane.montassir@gmail.com>
2023-10-02 16:19:31 -0700

    adding loop to print all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - prints all argumetns per line
+* @argc: number of arguments
+* @argv: command line arguments
+* Return: int
+*/
+
+int main(int argc, char *argv[])
+{
+	int c_arg; /* current_argument */
+
+	for (c_arg = 0; in < argc; c_arg++)
+	{
+		printf("%s\n", argv[c_arg]);
+	}
+	return (0);
+}
