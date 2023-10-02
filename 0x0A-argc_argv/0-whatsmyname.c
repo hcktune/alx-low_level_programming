33dae2fbec880d129619d01fe06f72325678dc5f
root <mohammed.aymane.montassir@gmail.com>
2023-10-02 16:07:24 -0700

    print argv[0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the name of current file
+* @argv: argument list
+* @argc: argument count
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
