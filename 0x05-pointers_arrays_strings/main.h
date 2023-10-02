43614932f5a321ba74229444b1b79378473e4f1a
Aymane <59456096+hcktune@users.noreply.github.com>
2023-10-02 22:53:37 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,3 +5,7 @@ int _strlen(char *str);
 void _puts(char *str);
 void print_rev(char *c);
 void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
