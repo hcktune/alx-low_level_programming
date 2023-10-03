60f19588f56ea3a062b62410c17de6c25076d5a7
root <mohammed.aymane.montassir@gmail.com>
2023-10-02 17:07:16 -0700

    add 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - adds positive numbers.
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	return (0);
+}
