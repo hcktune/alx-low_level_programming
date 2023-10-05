89fc5132222d997078fa18b01417db8187599b93
root <mohammed.aymane.montassir@gmail.com>
2023-10-04 18:00:46 -0700

    what s wroooong

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,9 +21,8 @@ int **alloc_grid(int w, int h)
 	if (arr == NULL)
 		return (NULL);
 	for (j = 0; j < h; j++)
-	{
 		arr[j] = (int *)malloc(w * sizeof(int));
-	}
+
 	for (j = 0; j < h; j++)
 	{
 		for (i = 0; i < w; i++)
