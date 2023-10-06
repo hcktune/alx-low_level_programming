d5b028e0271e662119348259d1c5b2d0abac04c3
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-06 23:37:59 +0100

    remove 0 stdlib stdio sizeof()

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
-/*
+/**
  * malloc_checked - check for any failure of malloc func
  * @s: size of memory (bytes)
  * Return: pointer
- **/
+ */
 
 void *malloc_checked(unsigned int s)
 {
 	void *p;
 	
-	p = malloc(sizeof(s));
+	p = malloc(s);
 	
 	if (p == NULL)
 		exit(98);
