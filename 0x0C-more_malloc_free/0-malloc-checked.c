ccfed0ae6c210952cd4a0e70148bdf03455d9e47
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-06 23:21:28 +0100

    adding 0 if p == null ==> exit(98)

---
+++ 0x0C-more_malloc_free/0-malloc-checked.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/*
+ * malloc_checked - check for any failure of malloc func
+ * @s: size of memory (bytes)
+ * Return: pointer
+ **/
+
+void *malloc_checked(unsigned int s)
+{
+	void *p;
+	
+	p = malloc(sizeof(s));
+	
+	if (p == NULL)
+		exit(98);
+
+	return (p)
+
+
+}
