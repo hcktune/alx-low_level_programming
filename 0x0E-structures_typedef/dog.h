be29ebfef003a99b5527ea82bdb2d07bd2ac4d3a
root <mohammed.aymane.montassir@gmail.com>
2023-10-10 15:40:53 -0700

    update dog"s struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
