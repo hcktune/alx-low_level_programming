61246166f8a92046e4256213175dcb9a02f6a837
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-11 19:21:07 +0100

    what about now

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,20 +1,20 @@
 #include "function_pointers.h"
 
 /**
- * int_index - search for function's index
+ * int_index - index of the function
  * @array: array of function
- * @size: array's size
- * @cmp: function pointer comparison
+ * @cmp: pointer comparison
+ * @size: arr's size
  * Return: Int
 */
 
-int int_index(int *array, size_t size, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || cmp == NULL || array == NULL )
+	if (size <= 0 || array == ((void *)0) || cmp == NULL)
 		return (-1);
-	
+
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
