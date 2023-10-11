838952e0a02c558e195d72f601ccd229850a7794
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-11 19:18:26 +0100

    trying to fix this

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, size_t size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || cmp == NULL ||  ((void *)0) == array )
+	if (size <= 0 || cmp == NULL || array == NULL )
 		return (-1);
 	
 	for (i = 0; i < size; i++)
