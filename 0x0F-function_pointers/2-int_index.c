e6ade64a631fccbd0681dd1b46e0cb7449103ec3
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-11 19:11:46 +0100

    void*

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, size_t size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || cmp == NULL || array == ((*void)0))
+	if (size <= 0 || cmp == NULL || array == ((void*)0))
 		return (-1);
 	
 	for (i = 0; i < size; i++)
