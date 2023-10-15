dfff9ef53e062a502612d1ed0dcff90efc2a4775
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-15 19:06:43 +0100

    easy print number then speratore must be null

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functiosns.h"
+
+/**
+ * print_numbers - output numbers in the params
+ * @separator: char or stirng to be printed between numbers
+ * @n: numbers count
+ * Return - void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list number;
+	va_start(number, n);
+	
+	while (i++ < n)
+	{
+		printf("%d", va_arg(number, int));
+
+		if (separator != NULL && i < n - 1) /* for not printing the last separator */
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(number);
+}
