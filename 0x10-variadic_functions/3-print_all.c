8c55a40395cc897025319fc1aed1a769dedb2f71
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-15 22:38:45 +0100

    fix compilation : by adding ")"

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -21,13 +21,13 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-					printf("%s%c", separator, (char)va_arg(spec, int);
+					printf("%s%c", separator, (char)va_arg(spec, int));
 					break;
 				case 'i':
-					printf("%s%d", separator, va_arg(sepc, int);
+					printf("%s%d", separator, va_arg(sepc, int));
 					break;
 				case 'f':
-					printf("%s%f", separator, va_arg(spec, double);
+					printf("%s%f", separator, va_arg(spec, double));
 					break;
 				case 's':
 					str_spec = va_arg(spec, char *);
