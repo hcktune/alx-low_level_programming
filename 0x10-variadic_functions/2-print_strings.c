92b3f6ab94eb94fc99851fe99dd76e3feb114388
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-15 21:29:02 +0100

    connecting if-else

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,8 +22,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", res);
-		/* end if */
-
 		if ((i < n - 1 ) && separator != NULL)
 			printf("%s", separator);
 	}
