e3f1d90d0918169c64843cab7003f7e43caa4b38
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-20 00:15:24 +0100

    fixing params

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,9 @@
 #include "lists.h"
-
 /**
  * list_len - length of lists's struct
  * @h: head
  * Return: the length of the list structure
 */
-
 size_t list_len(const list_t *h)
 {
 	const list_t *p = h;
@@ -13,8 +11,8 @@ size_t list_len(const list_t *h)
 
 	while (ptr != NULL)
 	{
-		lenght++;
+		len++;
 		p = p->next;
 	}
-	return (lenght);
+	return (len);
 }
