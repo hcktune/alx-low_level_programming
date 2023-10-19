fe35f9d078d94ae6704a20183e2f0b4222eb7902
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-20 00:26:03 +0100

    malloc list_t, list-next = *head, *head = list;

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+/**
+ * add_node - add a node to the list 
+ * @head: the head of the list 
+ * @str: node's name
+ * Return: address of the node
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *list;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	list = malloc(sizeof(list_t));
+	if (!list)
+		return (NULL);
+	list->str = strdup(str);
+	list->len = len;
+	list->next = *head;
+	*head = list;
+
+	return (*head);
+}
