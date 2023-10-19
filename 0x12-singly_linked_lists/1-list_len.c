f41f499bf69e0a43317764af3744a3be7b3aefee
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-20 00:13:57 +0100

    ez while ptr diff null, len++, p->netxt

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - length of lists's struct
+ * @h: head
+ * Return: the length of the list structure
+*/
+
+size_t list_len(const list_t *h)
+{
+	const list_t *p = h;
+	size_t len = 0;
+
+	while (ptr != NULL)
+	{
+		lenght++;
+		p = p->next;
+	}
+	return (lenght);
+}
