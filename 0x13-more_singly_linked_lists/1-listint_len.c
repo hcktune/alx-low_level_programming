e0e52a78f171485e3bed8f24521c3d0edd549239
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-24 22:13:47 +0100

    counting the length of a linked_list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - Counts the number of elements
+ * @h: Pointer to the head of the list.
+ * Return: int
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *node = h;
+	unsigned int c = 0;
+	
+	while (node)
+	{
+		node = node->next;
+		c++;
+	}
+	return (c);
+}
