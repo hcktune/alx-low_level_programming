faf816895122155ed7b4fa5792818b05442eff04
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-25 00:38:50 +0100

    sum all value

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum the all value 
+ * @head: ptr
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *node = head;
+	int sum = 0;
+
+	while (node)
+	{
+		sum += node->n;
+		node = node->next;
+	}
+
+	return (sum);
+}
