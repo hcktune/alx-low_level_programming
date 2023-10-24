45fe7dfda85ae6dfb757a42d48f403da2a44a80f
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-25 00:32:47 +0100

    add free ;

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete the head node of a listint_t 
+ * @head: ptr 
+ * Return: head node's data 
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *node = *head;
+	int val;
+
+	if (!head || !(*head))
+	{
+		return (0);
+	}
+
+
+	val = node->n;
+
+	*head = node->next;
+	node->next = NULL;
+	free(node);
+
+	return (value);
+}
