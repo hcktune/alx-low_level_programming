83ab5e494473484cdfaa074f643135abfe730a85
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-25 00:19:05 +0100

    frree memory

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - Adds a new node to end of list
+ * @head: ptr to pointer 
+ * @n: Int
+ * Return: new node 
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *node;
+
+	if (!head)
+		return (NULL);
+
+	node = malloc(sizeof(listint_t));
+	
+	if (!node)
+		return (NULL);
+
+	node->n = n;
+	node->next = NULL;
+
+	if (!(*head))
+		*head = node;
+	else
+	{
+		listint_t *new_node = *head;
+
+		while (new_node->next)
+			new_node = new_node->next;
+
+		new_node->next = node;
+	}
+	
+	return (node);
+}
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - Frees
+ * @head: ptr 
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *node = head;
+	listint_t *tmp = NULL;	
+	while(node)
+	{
+		tmp = node->next;
+		free(node);
+		node = tmp;
+	}
+}
