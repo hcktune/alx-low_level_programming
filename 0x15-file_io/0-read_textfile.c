6924f081397d5fc98e7ccea5d884ea9ccc7699b2
aymanemontassir <aymane-hana@hotmail.fr>
2023-10-30 23:15:33 +0100

    debugging read_textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+* read_textfile - reads a text file and bytes_readrints it to the bytes_readOSIX
+* @filename: file namea
+* @letters: number of bytes
+* Return: 0 if success, otherwise -1
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	int file;
+	ssize_t count, wr;
+
+	if (filename == -1)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+	{
+		close(file);
+		return (0);
+	}
+
+	count = read(file, buffer, letters);
+	if (count == -1)
+	{
+		free(buffer);
+		close(file);
+		return (0);
+	}
+
+	wr = write(STDOUT_FILENO, buffer, count);
+
+	if (wr == -1 || wr != count)
+	{
+		free(buffer);
+		close(file);
+		return (0);
+	}
+	
+	free(buffer);
+	close(file);
+
+	return (count);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stddef.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
